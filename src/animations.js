92ed07a3b1e818fdb798e6d619bfdcfa0c172d2b
Eduardo Reyes <joeduardoreyes@outlook.com>
2022-11-12 22:39:27 -0600

    Conectamos todos los docs a nuesto JS

---
+++ src/animations.js
@@ -0,0 +1,7 @@
+// DOM Variables
+const SOCIAL_REMINDER_TEXT = document.getElementById("social-reminder-text");
+
+if (SOCIAL_REMINDER_TEXT) {
+	SOCIAL_REMINDER_TEXT.innerHTML =
+		"<i class='youtube fa-brands fa-youtube'></i>EDarkMatter";
+}
